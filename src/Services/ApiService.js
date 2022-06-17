7f84431e1f26c5313ad440cc9779c65ecdd6d2e9
AbhishekGajera <abhishekgajera3140@gmail.com>
2022-06-18 03:29:22 +0530

    fix: code

--- src/Services/ApiService.js
+++ src/Services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const getAllProduct = (callback, errorcallback) => {
   axios
-    .get("http://localhost:8000/getproduct")
+    .get("http://167.99.13.120:8001/getproduct")
     .then((res) => {
       if (callback != null) {
         callback(res);
